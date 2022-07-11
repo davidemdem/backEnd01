fcb5d0627338fce06cd2f24177d5b58614f280f8
David Medina <demacdc6@gmail.com>
2022-07-11 00:10:42 -0300

    inclui vistas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
+const path=require('path');
 
 require('dotenv').config()
 
 const express=require('express')
 const app=express();
+
+app.get('/',function (solucitud,respuesta){
+respuesta.send('<h1>bienvenido!!!!!</h1>');
+});
+
+app.get('/contacto',function (solucitud,respuesta){
+    respuesta.sendFile(path.join(__dirname,'views/index.html'))
+});
 
 app.listen(process.env.PUERTO,function() {
     console.log('servidor iniciado en puerto:'+process.env.PUERTO)
